75456bf6a41ca78b3681db6f954bbf56031e4b84
Artem Meldziuk <retrosexyal@gmail.com>
2022-05-22 13:20:34 +0300

    feat: create new folder and coppy all files from old folder (sun 01:20 PM 22.05.2022)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFolder = path.join(__dirname, '/files');
+fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, (err)=>{
+    if (err) return console.log(err);
+});
+const pathToNewFolder = path.join(__dirname, '/files-copy');
 
+
+fs.readdir(pathToNewFolder,{ withFileTypes: true }, (err, files)=>{
+    if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {    
+      if (file.isFile()){      
+        fs.stat(path.join(pathToNewFolder, file.name), (error, stats)=>{
+          if (error) {
+            console.log(error);
+          } else {
+            fs.unlink(path.join(pathToNewFolder, file.name), (err)=>{
+                if (err) {
+                    console.log(err);
+                    }
+            })          
+          }
+        });        
+      }
+    });
+    };
+});
+
+fs.readdir(pathToFolder,{ withFileTypes: true }, (err, files)=>{
+    if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {    
+      if (file.isFile()){      
+        fs.stat(path.join(pathToFolder, file.name), (error, stats)=>{
+          if (error) {
+            console.log(error);
+          } else {
+              fs.copyFile(path.join(pathToFolder, file.name), path.join(pathToNewFolder, file.name), (err)=>{
+                if (err)
+                console.log(err);
+              })           
+          }
+        });        
+      }
+    });
+    };
+});
